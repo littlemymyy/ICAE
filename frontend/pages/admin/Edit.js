4d6b9b6d3870158f89b474c7d3e12d425b8d3932
mydminds <mydminds@littlemymyy.local>
2023-08-19 16:09:12 +0700

    frontend

---
+++ frontend/pages/admin/Edit.js
@@ -0,0 +1,154 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/layout/Navbar";
+import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import { Box, Typography } from "@mui/material";
+import { Fragment } from "react";
+import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import BuildIcon from '@mui/icons-material/Build';
+
+export default function Edit_Admin() {
+    return(
+       <>
+         <Navbar />
+         <Fragment>
+            <Box sx={{textAlign:"-webkit-center"}}>
+                <Box borderRadius={'5px'}
+                    display={'flex'}
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                    textAlign={'center'}
+                    height={'50px'}
+                    backgroundColor={'yellow'}
+                    width={'150px'}
+                    padding={'5px'}
+                    margin={'10px'}
+                    
+                
+                >
+
+                
+                
+
+                    <BuildIcon sx={{ color: "black"}}></BuildIcon>
+                    <Typography variant="h7" sx={{ color: "black"}}>แก้ไขสารเคมี</Typography>
+
+
+                </Box>
+            </Box>
+            <Box sx={{
+                display: {xs:'block' , md:'flex'},
+                backgroundColor: {xs:'' , md:'#F8F8F8 '},
+                borderRadius: {xs:'' , md:'25px'},
+                margin: {xs:'' , md:'10px 40px 10px 40px'},
+                padding: {xs:'10px 60px 10px 60px' , md:'10px 40px 10px 40px'},
+                justifyContent: {xs:'' , md:'space-between'},
+                gap: {xs:'' , md:'50px'}
+                
+    
+
+            }}
+            
+            >
+                <Box display={'grid'} gap={'10px'}>
+                    <Typography variant="h7">เพิ่มสารเคมี</Typography>
+                    <FormControl>
+                        
+                        <RadioGroup
+                        
+                            row
+                            aria-labelledby="demo-row-radio-buttons-group-label"
+                            name="row-radio-buttons-group"
+                        >
+                            <FormControlLabel value="Annex II" control={<Radio />} label="Annex II" />
+                            <FormControlLabel value="Annex III" control={<Radio />} label="Annex III" />
+                            <FormControlLabel value="Annex IV" control={<Radio />} label="Annex IV" />
+                            <FormControlLabel value="Annex V" control={<Radio />} label="Annex V" />
+                           
+                            
+                        </RadioGroup>
+                    </FormControl>
+                    <Typography variant="h7">ชื่อสารเคมี</Typography>
+                    <TextField label="ชื่อสารเคมี" variant="outlined" />
+                    <Typography variant="h7">รหัส CAS NO</Typography>
+                    <TextField label="รหัส CAS NO" variant="outlined" />
+                    <Typography variant="h7">คำอธิบาย</Typography>
+                    <TextField
+                        id="outlined-multiline-static"
+                        label="คำอธิบาย"
+                        multiline
+                        rows={4}
+                    />
+
+                    <Box display={'flex'}  gap={"20px"} marginBottom={'20px'}>
+                        <TextField label="ผลข้างเคียง" variant="outlined" />
+                        <TextField label="กำหนดปริมาณ" variant="outlined" />
+
+                    </Box>
+
+                </Box>
+            
+               
+                <Box 
+                    gap={'10px'}
+                    height={'500px'}
+                    width={'700px'}
+                    display={'block'}
+                    borderRadius={'5px'}
+                    alignItems={'center'}
+                    justifyContent={'center'}
+                    
+
+
+
+                
+                >
+                    <Box 
+                        border={'2px dashed grey'}
+                        textAlign={'center'}
+                        min-height={'100vh'}
+                        gap={'30px'}
+                        justifyContent={'center'}
+                        height={'450px'}
+                        width={'700px'}
+
+                    >
+                        <CloudDownloadIcon></CloudDownloadIcon>
+                        <header>ลากและวางไฟล์ที่นี่</header>
+                        <span>หรือ</span>
+                        <button>เลือกไฟล์</button>
+                        
+                    </Box>  
+                    
+
+                    <Box sx={{textAlign:'center', marginTop:'20px'}}>
+                        <button color="yellow">บันทึก</button>
+                    </Box>
+                        
+                    
+
+                    
+                   
+                    
+                                    
+                    
+                    
+
+                </Box>
+            
+            </Box>
+
+
+
+
+
+
+
+         </Fragment>
+         <Footer />
+       
+       </>
+    )
+
+}
---
+++ frontend/pages/index.js
@@ -0,0 +1,170 @@
+import { AppBar, Box, Container, Grid, Typography } from "@mui/material";
+import { Fragment } from "react";
+// import Navbar from "../components/layout/navbar";
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/Footer";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Fragment>
+        <Box
+          justifyContent={"space-around"}
+          position={"relative"}
+          z-index={"-1"}
+        >
+          {/* <Typography variant="h6" m="10px 0px 0px 20px">
+            news and updates
+          </Typography> */}
+
+          <Box>
+            <Swiper
+              spaceBetween={30}
+              pagination={{
+                clickable: true,
+              }}
+              Autoplay={true}
+              modules={[Pagination]}
+              className="mySwiper"
+            >
+              <SwiperSlide>
+                <img className="slide_pic" src="/news1.jpeg" />
+              </SwiperSlide>
+              <SwiperSlide>
+                <img className="slide_pic" src="/news2.jpeg" />
+              </SwiperSlide>
+            </Swiper>
+          </Box>
+        </Box>
+
+        <Box
+          m="10px 0px 10px 0px"
+          sx={{
+            justifyContent: { xs: "block", md: "space-around" },
+            display: { xs: "block", md: "flex" },
+            margin: { xs: "", md: "10px 0px 10px 0px" },
+            textAlign: { xs: "center", md: "" },
+          }}
+        >
+          <Box
+            sx={{
+              justifyContent: { xs: "center", md: "" },
+              textAlign: { xs: "center", md: "left" },
+              dispaly: { xs: "", md: "block" },
+							margin:{xs:'20px 0px 20px 0px' , md:'20px 0px 20px 0px'},
+							fontSize:{xs:'', md:'20px'},
+            }}
+          >
+            <p>ผู้ช่วยสูตร</p>
+					
+            <Box
+              sx={{
+                justifyContent: { xs: "center", md: "left" },
+                textAlign: { xs: "center", md: "left" },
+                alignItems: "center",
+                display: "flex",
+                margin: { xs: "0px 0px 5px 0px", md: "0px 0px 5px 0px" },
+              }}
+            >
+              <CheckCircleIcon
+                sx={{ color: "green", fontSize: 20, marginRight: "5px" }}
+              />
+              <span>ตรวจสอบสูตรด้วยข้อมูลเดียวกับ อย. ไทย</span>
+            </Box>
+            <Box
+              sx={{
+                justifyContent: { xs: "center", md: "left" },
+                textAlign: { xs: "center", md: "left" },
+                dispaly: { xs: "flex", md: "" },
+                margin: { xs: "0px 0px 5px 0px", md: "0px 0px 5px 0px" },
+              }}
+            >
+              <CheckCircleIcon
+                sx={{ color: "green", fontSize: 20, marginRight: "5px" }}
+              />
+              <span>รู้ข้อผิดพลาด</span>
+            </Box>
+            <Box
+              sx={{
+                justifyContent: { xs: "center", md: "left" },
+                textAlign: { xs: "center", md: "left" },
+                dispaly: { xs: "flex", md: "" },
+                margin: { xs: "0px 0px 5px 0px", md: "0px 0px 5px 0px" },
+              }}
+            >
+              <CheckCircleIcon
+                sx={{ color: "green", fontSize: 20, marginRight: "5px" }}
+              />
+              <span>การแก้ไขออนไลน์</span>
+            </Box>
+          </Box>
+
+          <Box
+            sx={{
+              justifyContent: { xs: "center", md: "" },
+              textAlign: { xs: "center", md: "" },
+              margin: { xs: "10px 0px 0px 0px", md: "" },
+            }}
+          >
+            <img src="/ability.png" style={{ maxWidth: 110 + "px" }} />
+          </Box>
+        </Box>
+
+        <Box
+          sx={{
+            display: { xs: "", md: "block" },
+            textAlign: { xs: "center", md: "center" },
+            padding: { xs: "30px 0px 30px 0px", md: "30px 0px 30px 0px" },
+            backgroundColor: { xs: "rgb(219,233,245)", md: "rgb(219,233,245)" },
+						
+						
+          }}
+        >
+          <Typography sx={{
+							fontSize:{md:'30px',xs:'20px'},
+					}} 
+					
+					>ข้อได้เปรียบหลัก</Typography>
+          <Grid
+            container
+            sx={{
+              textAlign: { xs: "", md: "center" },
+							margin:{xs:'20px 0px 0px 0px' , md:'20px 0px 0px 0px'},
+							fontSize:{xs:'', md:'20px'}
+            }}
+          >
+            <Grid item xs={4} sx={{
+							fontSize:{xs:'', md:'20px'}
+						}} >
+              <img src="/adven.png" style={{ maxWidth: 30 + "px" }} />
+              <p>ข้อมูลที่ครอบคลุมและถูกต้อง</p>
+            </Grid>
+            <Grid item xs={4} >
+              <img src="/adven2.png" style={{ maxWidth: 30 + "px" }} />
+              <p>ตรวจสอบก่อนยื่นจริง</p>
+            </Grid>
+            <Grid item xs={4}>
+              <img src="/adven3.png" style={{ maxWidth: 30 + "px" }} />
+              <p>สมาร์ทครบวงจรในที่เดียวและสะดวกสบาย</p>
+            </Grid>
+          </Grid>
+        </Box>
+      </Fragment>
+      <Footer></Footer>
+      
+    </>
+  );
+}
